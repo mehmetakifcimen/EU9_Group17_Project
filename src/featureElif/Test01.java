616afe5185ccbfca16286c369423332a2055a29e
elifdmrr <elif.24.urek@gmail.com>
2022-07-31 21:01:04 +0200

    featureElif added

--- src/featureElif/Test01.java
+++ src/featureElif/Test01.java
@@ -1,4 +1,18 @@
 package featureElif;
 
 public class Test01 {
-}
+    public static void main(String[] args) {
+
+
+
+        String str = "Hello guys do you see this ?";
+
+    }
+
+
+
+
+    }
+
+
+
